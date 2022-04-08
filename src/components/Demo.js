540261ddba8fabd09e43163935a6a7ce5746f042
Abhijeet Kumar <63132585+Abhijeet1520@users.noreply.github.com>
2022-04-08 23:37:48 +0530

    Add google maps

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,6 +1,30 @@
 import React, { Component } from "react";
+import { compose, withProps } from "recompose";
+import {
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+    Marker,
+} from "react-google-maps";
+
+const MyMapComponent = compose(
+    withProps({
+        googleMapURL:
+            "https://maps.googleapis.com/maps/api/js?key=AIzaSyBzl1ZmweiFDXewxbMlA264PWo1NrqbKyE&v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `400px` }} />,
+        mapElement: <div style={{ height: `100%` }} />,
+    }),
+    withScriptjs,
+    withGoogleMap
+)((props) => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+        <Marker position={{ lat: -34.397, lng: 150.644 }} />
+    </GoogleMap>
+));
 
 class Demo extends Component {
+    ReactGoogleMaps = () => [<MyMapComponent key="map" />];
     render() {
         return (
             <section
@@ -9,12 +33,15 @@ class Demo extends Component {
                 style={{
                     height: "100vh",
                     width: "auto",
-                    background: "red",
                 }}
             >
-                <div className="container">
-                    <h1>Hello Demo</h1>
-                </div>
+                <MyMapComponent
+                    key="map"
+                    style={{
+                        height: "100%",
+                        width: "100%",
+                    }}
+                />
             </section>
         );
     }
